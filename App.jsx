7403f4878f3368c4d15ca77f6eaacbb390ce8101
Subrato Kumar Verma <138965935+18subrato@users.noreply.github.com>
2024-10-02 16:17:27 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Todo from "./Components/Todo"
+
+function App() {
+  
+
+  return (
+    <>
+     <div className="bg-gradient-to-br from-orange-500 to-blue-500 min-h-screen grid py-4">
+        <Todo/>
+     </div>
+    </>
+  )
+}
+
+export default App
